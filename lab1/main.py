22984704a1bc28c49a4d847e427e085ba59c73dd
LeoMitinskyi <73614899+LeoMitinskyi@users.noreply.github.com>
2021-11-30 14:16:29 +0300

    Update main.py

--- lab1/main.py
+++ lab1/main.py
@@ -10,11 +10,11 @@ df = read_data('/data/notebook_files', 'train.csv')
 df.head()
 
 def load_dataset(label_dict):
-    train_X = read_data('train.csv').values[:,:-2]
-    train_y = read_data('train.csv')['Activity']
+    train_X = read_data('data/train.csv').values[:,:-2]
+    train_y = read_data('data/train.csv')['Activity']
     train_y = train_y.map(label_dict).values
-    test_X = read_data('/data/notebook_files', 'test.csv').values[:,:-2]
-    test_y = read_data('/data/notebook_files', 'test.csv')
+    test_X = read_data('data/test.csv').values[:,:-2]
+    test_y = read_data('data/test.csv')
     test_y = test_y['Activity'].map(label_dict).values
     return train_X, train_y, test_X, test_y
 label_dict = {'WALKING':0, 'WALKING_UPSTAIRS':1, 'WALKING_DOWNSTAIRS':2, 'SITTING':3, 'STANDING':4, 'LAYING':5}
